3cd8d094a1c1461edbca7aadcfa7015334e7337f
seansolleder <sean@totalscale.com>
2025-03-26 17:36:05 -0700

    Various Chnages

---
+++ Rail/AccessEndicottt/AccessEndicottt/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AccessEndicottt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBoxTables = new ComboBox();
+            dataGridView = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBoxTables
+            // 
+            comboBoxTables.FormattingEnabled = true;
+            comboBoxTables.Location = new Point(12, 12);
+            comboBoxTables.Name = "comboBoxTables";
+            comboBoxTables.Size = new Size(244, 23);
+            comboBoxTables.TabIndex = 0;
+            comboBoxTables.SelectedIndexChanged += comboBoxTables_SelectedIndexChanged;
+            // 
+            // dataGridView
+            // 
+            dataGridView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(12, 57);
+            dataGridView.Name = "dataGridView";
+            dataGridView.Size = new Size(776, 381);
+            dataGridView.TabIndex = 1;
+            dataGridView.DataError += dataGridView_DataError;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView);
+            Controls.Add(comboBoxTables);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox comboBoxTables;
+        private DataGridView dataGridView;
+    }
+}
